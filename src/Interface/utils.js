ca7eb11454109be37a83011fb69e6277f038c767
Laura-Nyaaga <nyaagaaluoch@gmail.com>
2024-06-28 19:31:11 +0300

    add files

---
+++ src/Interface/utils.js
@@ -0,0 +1,10 @@
+
+// const baseurl = process.env.REACT_APP_BASE_URL;
+// const apikey = process.env.REACT_APP_API_KEY;
+
+
+// export const interface  =  async () => {
+//     try{
+//         const response = await fetch( `${baseurl}v1/forecast.json?key=${apikey}`)
+//     }
+// }
